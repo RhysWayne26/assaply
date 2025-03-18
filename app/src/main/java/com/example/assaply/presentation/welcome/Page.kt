7d082bccd6ab9a86248b3589cebdad062eda749d
George <rhyswayne2601@gmail.com>
2025-03-18 22:44:03 +0300

    Fix pages description

--- app/src/main/java/com/example/assaply/presentation/welcome/Page.kt
+++ app/src/main/java/com/example/assaply/presentation/welcome/Page.kt
@@ -6,7 +6,7 @@ import com.example.assaply.R
 data class Page(val title: String, val description: String = "", @DrawableRes val image: Int)
 
 val pages = listOf(
-    Page("Welcome", image = R.drawable.welcum),
-    Page("Only the newest news right here.", "Enjoy!", R.drawable.nightcity)
+    Page("Welcome", "to Assaply", image = R.drawable.welcum),
+    Page("Only relevant", "hi-tech and politics news. Enjoy!", R.drawable.nightcity)
 )
 //optimized
