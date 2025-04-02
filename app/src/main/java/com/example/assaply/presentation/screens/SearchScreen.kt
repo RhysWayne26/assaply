4746d234a3e1f7ae3ff28f1a7b08d36d1ee33db8
George <rhyswayne2601@gmail.com>
2025-04-02 18:31:02 +0300

    Add constrait layout to search screen

--- app/src/main/java/com/example/assaply/presentation/screens/SearchScreen.kt
+++ app/src/main/java/com/example/assaply/presentation/screens/SearchScreen.kt
@@ -1,14 +1,13 @@
 package com.example.assaply.presentation.screens
 
 import ArticlesList
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.example.assaply.data.domain.entities.Article
 import com.example.assaply.presentation.common.SearchBar
@@ -20,33 +19,55 @@ import com.example.assaply.util.Dimensions.MediumPadding1
 @Composable
 fun SearchScreen(
     state: SearchState,
-    event:(SearchEvent) -> Unit,
-    navigateToDetails:(Article) -> Unit
+    event: (SearchEvent) -> Unit,
+    navigateToDetails: (Article) -> Unit
 ) {
-
-    Column(
+    ConstraintLayout(
         modifier = Modifier
-            .padding(top = MediumPadding1, start = MediumPadding1, end = MediumPadding1)
             .statusBarsPadding()
+            .padding(
+                top = MediumPadding1,
+                start = MediumPadding1,
+                end = MediumPadding1
+            )
     ) {
+        val (searchBarRef, spacerRef, listRef) = createRefs()
+
         SearchBar(
             text = state.searchQuery,
             readOnly = false,
             onValueChange = { event(SearchEvent.UpdateSearchQuery(it)) },
-            onSearch = {
-                event(SearchEvent.SearchNews)
+            onSearch = { event(SearchEvent.SearchNews) },
+            modifier = Modifier.constrainAs(searchBarRef) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
             }
         )
-        Spacer(modifier = Modifier.height(MediumPadding1))
+
+        androidx.compose.foundation.layout.Spacer(
+            modifier = Modifier
+                .height(MediumPadding1)
+                .constrainAs(spacerRef) {
+                    top.linkTo(searchBarRef.bottom)
+                }
+        )
+
         state.articles?.let {
             val articles = it.collectAsLazyPagingItems()
             ArticlesList(
                 articles = articles,
-                onClick = navigateToDetails
+                onClick = navigateToDetails,
+                modifier = Modifier.constrainAs(listRef) {
+                    top.linkTo(spacerRef.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
             )
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
